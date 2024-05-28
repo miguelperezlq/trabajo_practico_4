94616258275b70599626a50d997855546b00be5b
miguel <miguel@localhost>
2024-05-27 23:59:09 -0300

    Pagina terminada con todos los requisitos solicitados

--- tp4_PerezJoseMiguel/src/main/java/ar/edu/unju/fi/controler/MainController.java
+++ tp4_PerezJoseMiguel/src/main/java/ar/edu/unju/fi/controler/MainController.java
@@ -1,5 +1,18 @@
 package ar.edu.unju.fi.controler;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/crud")
 public class MainController {
 
+    @GetMapping("/index")
+    public String getIndexPage() {
+        return "index"; // (archivo HTML) a renderizar
+    }
+
+
 }
+
